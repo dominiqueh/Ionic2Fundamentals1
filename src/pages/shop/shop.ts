052ca9816e93782aa812f98bb7e19d65e8793c4c
dominiqueh <dominiquehorner@gmail.com>
2017-03-09 13:42:15 -0700

    adding pages for shop products'

--- src/pages/shop/shop.ts
+++ src/pages/shop/shop.ts
@@ -1,19 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { NavController, NavParams } from 'ionic-angular';
-
-import { BuyoutPage } from "../buy/buyout";
+import { NavParams, NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-shop',
   templateUrl: 'shop.html'
 })
-export class ShopPage {
+export class ShopPage implements OnInit {
+  product: string;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public  navParams: NavParams,
+                      navCtrl: NavController) {}
 
   onGoToBuyout(){
-    this.navCtrl.push(BuyoutPage);
+    this.navCtrl.push(productName);
   }
 
+  onGoBack(){
+    this.navCtrl.popToRoot();
+  }
 }
